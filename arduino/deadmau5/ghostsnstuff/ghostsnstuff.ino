5bfbf031885ae15624e28791b978988d2d6fe232
ryanebrock <rbrock@zebra.com>
2015-10-27 06:31:03 -0500

    just commit what I have so far to keep it safe. This is nothing final by any means

--- arduino/deadmau5/ghostsnstuff/ghostsnstuff.ino
+++ arduino/deadmau5/ghostsnstuff/ghostsnstuff.ino
@@ -17,7 +17,7 @@ enum OP_TYPE {
   DELAY = 0x00,
   LOOP = 0x01,
   LED = 0x02,
-  EQIZR = 0x03,
+  EQUALIZER = 0x03,
   FADEIN = 0x04,
   FADEOUT = 0x05,
   FINISHED = 0xFF
@@ -31,12 +31,18 @@ struct BEAM {
   byte b;
 };
 
+struct FADE_PARAMS {
+  uint16_t fadeTime;
+  uint8_t brightness;
+  BEAM beam;
+};
+
 struct LOOP_PARAMS {
   byte operations;
   uint16_t count;
 };
 
-struct EQUALIZER {
+struct EQUALIZER_PARAMS {
   byte saddr;
   byte eaddr;
 };
@@ -48,48 +54,116 @@ struct OP {
     BEAM beam;
     uint16_t delayInMs;
     LOOP_PARAMS loopParams;
-    EQUALIZER equalizerRange;
+    EQUALIZER_PARAMS equalizerRange;
+    FADE_PARAMS fadeParams;
   } params;
 };
 
 
-const OP operations[] PROGMEM = {
-  /*{LOOP, {.loopParams = {1, 10}}},*/
-  {DELAY, {.delayInMs = 109}},
+#define RIGHT_EAR_B1_SADDR 0
+#define RIGHT_EAR_B1_EADDR 6
+#define RIGHT_EAR_B2_SADDR 7
+#define RIGHT_EAR_B2_EADDR 15
+#define RIGHT_EAR_B3_SADDR 16
+#define RIGHT_EAR_B3_EADDR 21
+#define RIGHT_EAR_B4_SADDR 22
+#define RIGHT_EAR_B4_EADDR 30
+#define RIGHT_EAR_B5_SADDR 31
+#define RIGHT_EAR_B5_EADDR 37
 
-  /*// Initial Beat - alternate eyes
-  {LED, 0, 2, 0xFF, 0, 0},
-  {DELAY, 250},
-  {LED, 0, 2, 0, 0, 0},
-  {DELAY, 216},
-  {LED, 3, 5, 0, 0, 0xFF},
-  {DELAY, 250},
-  {LED, 3, 5, 0, 0, 0},
-  {DELAY, 216},
-  {LOOP, 8, 10},*/
+#define RIGHT_CHEEK_SADDR 38
+#define RIGHT_CHEEK_EADDR 43
+
+#define RIGHT_EYE_ADDR   44
+
+#define FACE_B1_SADDR 45
+#define FACE_B1_EADDR 51
+#define FACE_B2_SADDR 52
+#define FACE_B2_EADDR 58
+#define FACE_B3_SADDR 59
+#define FACE_B3_EADDR 65
+
+#define LEFT_CHEEK_SADDR 66
+#define LEFT_CHEEK_EADDR 71
+
+#define LEFT_EYE_ADDR    72
+
+#define LEFT_EAR_B1_SADDR 73
+#define LEFT_EAR_B1_EADDR 79
+#define LEFT_EAR_B2_SADDR 80
+#define LEFT_EAR_B2_EADDR 88
+#define LEFT_EAR_B3_SADDR 89
+#define LEFT_EAR_B3_EADDR 94
+#define LEFT_EAR_B4_SADDR 95
+#define LEFT_EAR_B4_EADDR 103
+#define LEFT_EAR_B5_SADDR 104
+#define LEFT_EAR_B5_EADDR 110
+
+#define MOUTH_UPPER_SADDR 111
+#define MOUTH_UPPER_EADDR 129
+
+#define MOUTH_LOWER_SADDR 130
+#define MOUTH_LOWER_EADDR 147
+
+#define MOUTH_SADDR MOUTH_UPPER_SADDR
+#define MOUTH_EADDR MOUTH_LOWER_EADDR
+
+const OP operations[] PROGMEM = {
+  {DELAY, {.delayInMs = 110}},
+
+  // Initial Beat until intro fade in - alternate eyes
+ /* {LOOP, {.loopParams = {8, 16}}},
+  {LED, {.beam = {LEFT_EYE_ADDR, LEFT_EYE_ADDR, 0xFF, 0, 0}}},
+  {DELAY, {.delayInMs = 250}},
+  {LED, {.beam = {LEFT_EYE_ADDR, LEFT_EYE_ADDR, 0, 0, 0}}},
+  {DELAY, {.delayInMs = 221}},
+  {LED, {.beam = {RIGHT_EYE_ADDR, RIGHT_EYE_ADDR, 0, 0, 0xFF}}},
+  {DELAY, {.delayInMs = 250}},
+  {LED, {.beam = {RIGHT_EYE_ADDR, RIGHT_EYE_ADDR, 0, 0, 0}}},
+  {DELAY, {.delayInMs = 222}},*/
+
+  // 15250ms is when the first chord starts
+  {LED, {.beam = {LEFT_EYE_ADDR, LEFT_EYE_ADDR, 0xFF, 0, 0}}}, // Beat
+  {FADEIN, {.fadeParams = {250, 0x7F, 6, 7, 0, 0xFF, 0}}}, //Fade first half
+  {LED, {.beam = {LEFT_EYE_ADDR, LEFT_EYE_ADDR, 0, 0, 0}}}, // Beat off
+  {FADEIN, {.fadeParams = {221, 0xFF, 6, 7, 0, 0xFF, 0}}}, //Fade second half
+
+  // Three more beats until 17127
+  /*{LED, {.beam = {RIGHT_EYE_ADDR, RIGHT_EYE_ADDR, 0, 0, 0xFF}}},
+  {DELAY, {.delayInMs = 250}},
+  {LED, {.beam = {RIGHT_EYE_ADDR, RIGHT_EYE_ADDR, 0, 0, 0}}},
+  {DELAY, {.delayInMs = 222}},
+  {LED, {.beam = {LEFT_EYE_ADDR, LEFT_EYE_ADDR, 0xFF, 0, 0}}},
+  {DELAY, {.delayInMs = 250}},
+  {LED, {.beam = {LEFT_EYE_ADDR, LEFT_EYE_ADDR, 0, 0, 0}}},
+  {DELAY, {.delayInMs = 221}},
+  {LED, {.beam = {RIGHT_EYE_ADDR, RIGHT_EYE_ADDR, 0, 0, 0xFF}}},
+  {DELAY, {.delayInMs = 250}},
+  {LED, {.beam = {RIGHT_EYE_ADDR, RIGHT_EYE_ADDR, 0, 0, 0}}},
+  {DELAY, {.delayInMs = 222}},*/
+  
+  // 17127 is when the second chord starts (diff 1877)
+
+  // 17622 is when the third chord starts (diff 495)
+
+  // 18630 is when the forth chord starts (diff 1008)
+
+  // Start over
+
+  // 22744 
+  // 24621 
+  // 25116
+  // 26124 
+
+
+
+
+  //30111 - starts beat fading
+  // 
+  
   {FINISHED, 0}
 };
 #define END_OF_OPERATIONS ((byte *)operations) + sizeof(operations)
-
-/*
- * const byte operations[] PROGMEM = {
-  DELAY, 109,
-
-  // Initial Beat - alternate eyes
-  LED, 0, 2, 0xFF, 0, 0,
-  DELAY, 250,
-  LED, 0, 2, 0, 0, 0,
-  DELAY, 216,
-  LED, 3, 5, 0, 0, 0xFF,
-  DELAY, 250,
-  LED, 3, 5, 0, 0, 0,
-  DELAY, 216,
-  LOOP, 8, 10,
-  FINISHED, 0
-};
-#define END_OF_OPERATIONS = (operations + sizeof(operations))
-
- */
 
 void setup() {
   strip.begin();
@@ -98,15 +172,6 @@ void setup() {
   Serial.print("Ready ...");
 }
 
-/*enum OP_TYPE {
-  DELAY = 0x00,
-  LOOP = 0x01,
-  LED = 0x02,
-  EQIZR = 0x03,
-  FADEIN = 0x04,
-  FADEOUT = 0x05,
-  FINISHED = 0xFF
-}*/
 
 void loop() {
   uint32_t tickDrift = millis();
@@ -120,17 +185,18 @@ void loop() {
 
     // Get the operation Type 
     memcpy_P(&mainOperationType, mainOperationPC, 1);
-    Serial.print("Main Operation Type: "); Serial.println(mainOperationType);
+    //Serial.print("Main Operation Type: "); Serial.println(mainOperationType);
     
     if (mainOperationType == LOOP) {
       memcpy_P(&loopParams, mainOperationPC + 1, sizeof(loopParams));
-      Serial.print("Loop operations: "); Serial.println(loopParams.operations);
-      Serial.print("Loop back count: "); Serial.println(loopParams.count);
+      /*Serial.print("Loop operations: "); Serial.println(loopParams.operations);
+      Serial.print("Loop back count: "); Serial.println(loopParams.count);*/
 
       // Adjust program counter
       mainOperationPC += sizeof(OP);
     } else if (mainOperationType == FINISHED) {
-      Serial.println("Shutting Down");
+      uint32_t currentExitTime = millis();
+      Serial.print("Shutting Down: "); Serial.println(currentExitTime);
       delay(10000);
       exit(0);
     }
@@ -139,7 +205,7 @@ void loop() {
     byte *loopOperationsPC;
     
     for(uint16_t i = 0; i < loopParams.count; i++) {
-      Serial.print("Loop Iteration: "); Serial.println(i);
+     // Serial.print("Loop Iteration: "); Serial.println(i);
       loopOperationsPC = mainOperationPC;
       byte *endOfLoopPC = loopOperationsPC + (loopParams.operations * sizeof(OP));
     
@@ -148,7 +214,7 @@ void loop() {
   
         // Get the operation Type 
         memcpy_P(&operationType, loopOperationsPC, 1);
-        Serial.print("Operation Type: "); Serial.println(operationType);
+        //Serial.print("Operation Type: "); Serial.println(operationType);
 
         byte *operationStructOffset = loopOperationsPC + 1;
       
@@ -156,9 +222,55 @@ void loop() {
           case DELAY: {
             uint16_t delayInMs = 0;
             memcpy_P(&delayInMs, operationStructOffset, sizeof(uint16_t));
-            Serial.print("Delay: "); Serial.println(delayInMs);
+           // Serial.print("Delay: "); Serial.println(delayInMs);
             
             delay(delayInMs);
+            break;
+          }
+          case LED: {
+            BEAM beamParams;
+            memcpy_P(&beamParams, operationStructOffset, sizeof(BEAM));
+            /*Serial.print("BEAM Start: "); Serial.println(beamParams.saddr);
+            Serial.print("BEAM End: "); Serial.println(beamParams.eaddr);
+            Serial.print("BEAM R: "); Serial.println(beamParams.r);
+            Serial.print("BEAM G: "); Serial.println(beamParams.g);
+            Serial.print("BEAM B: "); Serial.println(beamParams.b);*/
+
+            for(uint16_t addr = beamParams.saddr; addr <= beamParams.eaddr; addr++) {
+              strip.setPixelColor(addr, beamParams.r, beamParams.g, beamParams.b); 
+            }
+            strip.show();
+            break;
+          }
+          case EQUALIZER: {
+            EQUALIZER_PARAMS equalizerParams;
+            memcpy_P(&equalizerParams, operationStructOffset, sizeof(EQUALIZER_PARAMS));
+            Serial.print("EQUAL Start: "); Serial.println(equalizerParams.saddr);
+            Serial.print("EQUAL End: "); Serial.println(equalizerParams.eaddr);
+            break;
+          }
+          case FADEIN: {
+            FADE_PARAMS fadeParams;
+            memcpy_P(&fadeParams, operationStructOffset, sizeof(FADE_PARAMS));
+            /*Serial.print("FADE IN Duration: "); Serial.println(fadeParams.fadeTime);
+            Serial.print("FADE IN Brightness: "); Serial.println(fadeParams.brightness);
+            Serial.print("FADE IN Start: "); Serial.println(fadeParams.beam.saddr);
+            Serial.print("FADE IN End: "); Serial.println(fadeParams.beam.eaddr);
+            Serial.print("FADE IN R: "); Serial.println(fadeParams.beam.r);
+            Serial.print("FADE IN G: "); Serial.println(fadeParams.beam.g);
+            Serial.print("FADE IN B: "); Serial.println(fadeParams.beam.b);*/
+            delay(fadeParams.fadeTime);
+            break;
+          }
+          case FADEOUT: {
+            FADE_PARAMS fadeParams;
+            memcpy_P(&fadeParams, operationStructOffset, sizeof(FADE_PARAMS));
+            Serial.print("FADE OUT Duration: "); Serial.println(fadeParams.fadeTime);
+            Serial.print("FADE OUT Start: "); Serial.println(fadeParams.beam.saddr);
+            Serial.print("FADE OUT End: "); Serial.println(fadeParams.beam.eaddr);
+            Serial.print("FADE OUT R: "); Serial.println(fadeParams.beam.r);
+            Serial.print("FADE OUT G: "); Serial.println(fadeParams.beam.g);
+            Serial.print("FADE OUT B: "); Serial.println(fadeParams.beam.b);
             break;
           }
           case LOOP:
@@ -173,7 +285,7 @@ void loop() {
         loopOperationsPC += sizeof(OP);
       } while (loopOperationsPC < endOfLoopPC);
 
-      Serial.println("Done with loop code block, looping back");
+      //Serial.println("Done with loop code block, looping back");
     }
 
     mainOperationPC = loopOperationsPC;
@@ -181,11 +293,11 @@ void loop() {
     // Reset loop params
     loopParams.count = loopParams.operations = 1;
     
-//strip.setPixelColor(j, op.r, op.g, op.b); 
-    //strip.show();
+
   } while (mainOperationPC < END_OF_OPERATIONS);
 
-  Serial.println("Shutting Down");
+  uint32_t currentExitTime = millis();
+  Serial.print("Shutting Down: "); Serial.println(currentExitTime);
   delay(10000);
   exit(0);
 }
--- java/ledmapcreator/src/org/brock/arduino/MapBuilder.java
+++ java/ledmapcreator/src/org/brock/arduino/MapBuilder.java
@@ -54,6 +54,71 @@ public class MapBuilder {
         return this;
     }
 
+    public MapBuilder equilizerBeat(int startTick, int endTick, int freqInMs, int saddr, int eaddr, int brightness) {
+        int tick = startTick;
+
+        int numOfLeds = eaddr - saddr;
+
+        if (numOfLeds < 3) {
+            throw new IllegalArgumentException("The number of LEDs in the Equalizer must be at least 3");
+        }
+
+        // Green is bottom 50%
+        // Yellow is Middle - Top 33.33%
+        // Red is top 16.67%
+        int numOfRed = numOfLeds / 6;
+        if (numOfRed <= 0) {
+            numOfRed = 1;
+        }
+
+        int numOfYellow = numOfLeds / 3;
+        if (numOfYellow <= 0) {
+            numOfYellow = 1;
+        }
+
+        int numOfGreen = numOfLeds / 2;
+        if (numOfGreen <= 0) {
+            numOfGreen = 1;
+        }
+
+
+        int leftOver = numOfLeds -  (numOfGreen + numOfRed + numOfYellow);
+        numOfGreen += leftOver;
+
+
+
+        while(tick < endTick) {
+            int addrOffset = saddr;
+            int addrEnd = saddr + numOfGreen - 1;
+            int tickDelay = tick;
+
+            for(int i = 0; i < numOfGreen; i++) {
+                operations.add(new Operation(tickDelay, addrOffset, addrEnd, brightness, 0, 0xFF, 0));
+                tickDelay += 15;
+            }
+
+            addrOffset += numOfGreen;
+            addrEnd += numOfYellow;
+
+            for(int i = 0; i < numOfGreen; i++) {
+                operations.add(new Operation(tickDelay, addrOffset, addrEnd, brightness, 0xFF, 0xFF, 0));
+                tickDelay += 15;
+            }
+            addrOffset += numOfYellow; addrEnd += numOfRed;
+
+            for(int i = 0; i < numOfGreen; i++) {
+                operations.add(new Operation(tickDelay, addrOffset, addrEnd, brightness, 0xFF, 0, 0));
+                tickDelay += 15;
+            }
+
+            operations.add(new Operation(tick + BEAT_BLAST_LENGTH, saddr, eaddr, 0, 0, 0, 0));
+
+            tick += freqInMs;
+        }
+
+        return this;
+    }
+
     public void build() {
         Collections.sort(operations);
 
@@ -107,18 +172,21 @@ public class MapBuilder {
     public static void main(String args[]) {
         MapBuilder m = new MapBuilder();
 
-        m.beat(109, 29000, 937, 0, 0, FULL_BRIGHTNESS, Color.RED)
-                .beat(576, 29000, 937, 1, 1, FULL_BRIGHTNESS, Color.BLUE)
-                .ramp(15260, 17500, 2, 4, FULL_BRIGHTNESS, Color.GREEN)
-                .ramp(17141, 19000, 5, 7, FULL_BRIGHTNESS, Color.MAGENTA)
-                .ramp(18070, 20000, 8, 10, FULL_BRIGHTNESS, Color.RED)
-                .ramp(18669, 21000, 11, 13, FULL_BRIGHTNESS, Color.BLUE)
-                //.ramp(22807, 25000, 14, 15, FULL_BRIGHTNESS, Color.GREEN)
-                .clear(22500)
-                .ramp(22807, 25500, 2, 4, FULL_BRIGHTNESS, Color.GREEN)
-                .ramp(24688, 27000, 5, 7, FULL_BRIGHTNESS, Color.MAGENTA)
-                .ramp(25617, 28000, 8, 10, FULL_BRIGHTNESS, Color.RED)
-                .ramp(26216, 29000, 11, 13, FULL_BRIGHTNESS, Color.BLUE)
-                .build();
+//        m.beat(109, 29000, 937, 0, 0, FULL_BRIGHTNESS, Color.RED)
+//                .beat(576, 29000, 937, 1, 1, FULL_BRIGHTNESS, Color.BLUE)
+//                .ramp(15260, 17500, 2, 4, FULL_BRIGHTNESS, Color.GREEN)
+//                .ramp(17141, 19000, 5, 7, FULL_BRIGHTNESS, Color.MAGENTA)
+//                .ramp(18070, 20000, 8, 10, FULL_BRIGHTNESS, Color.RED)
+//                .ramp(18669, 21000, 11, 13, FULL_BRIGHTNESS, Color.BLUE)
+//                //.ramp(22807, 25000, 14, 15, FULL_BRIGHTNESS, Color.GREEN)
+//                .clear(22500)
+//                .ramp(22807, 25500, 2, 4, FULL_BRIGHTNESS, Color.GREEN)
+//                .ramp(24688, 27000, 5, 7, FULL_BRIGHTNESS, Color.MAGENTA)
+//                .ramp(25617, 28000, 8, 10, FULL_BRIGHTNESS, Color.RED)
+//                .ramp(26216, 29000, 11, 13, FULL_BRIGHTNESS, Color.BLUE)
+//                .build();
+
+
+        m.equilizerBeat(109, 320000, 468, 0, 6, FULL_BRIGHTNESS).build();
     }
 }
