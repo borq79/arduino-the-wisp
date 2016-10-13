101036f428b91a57823dc469214e3013191dc6d7
ryanebrock <ryan.e.brock@gmail.com>
2016-10-12 21:09:16 -0500

    Update for pack meeting

--- arduino/stranger_things/teeshirt/teeshirt.ino
+++ arduino/stranger_things/teeshirt/teeshirt.ino
@@ -1,11 +1,12 @@
 #include <Adafruit_NeoPixel.h>
 
 
-#define PIXEL_DATA_PIN 1
-#define NUM_PIXEL      50
+#define PIXEL_DATA_PIN 9
+#define NUM_PIXEL      26
 
-#define ON_DURATION          2000
-#define BETWEEN_LETTER_PAUSE 1500
+#define ON_DURATION            1500
+#define BETWEEN_LETTER_PAUSE   1000
+#define PARTY_MODE_ITERATIONS  (5 * 60000) / (ON_DURATION * 2)
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
@@ -38,7 +39,7 @@ struct LIGHT {
   COLOR color;
 };
 
-#define BRIGHTNESS 0x1F
+#define BRIGHTNESS 0xDF
 
 // Offset is the letter (e.g. A = 0, B = 1)
 // Value is the LED address (e.g. 0 is the first light, 1 is the second, etc.)
@@ -72,8 +73,43 @@ LIGHT PIN_LETTER_MAP[] = { {0,  WHITE},    // A
                            {25, PURPLE}    // Z
   };
 
-char* PHRASES[]={"Right Here", "Run", "Happy Halloween"};
+// Offset is the letter (e.g. A = 0, B = 1)
+// Value is the LED address (e.g. 0 is the first light, 1 is the second, etc.)
+// This allows you to map the address on the light strand to the letter - note the lights wrap
+// at letter H in the movie, so while H is obviously after G, numerically in the strand it is not
+// This strand map is for a shirt that starts at Z rather than A so that you can wire it up to exit
+// at the bottom of the shirt
+LIGHT PIN_LETTER_MAP_BOTTOM_UP [] = {{25,  RED},    // A
+                                     {24,  GREEN},  // B
+                                     {23,  BLUE},   // C
+                                     {22,  YELLOW}, // D
+                                     {21,  RED},    // E
+                                     {20,  GREEN},  // F
+                                     {19,  BLUE},   // G
+                                     {18,  YELLOW}, // H
+                                     {9,   YELLOW}, // I
+                                     {10,  BLUE},   // J
+                                     {11,  GREEN},  // K
+                                     {12,  RED},    // L
+                                     {13,  YELLOW}, // M
+                                     {14,  BLUE},   // N
+                                     {15,  GREEN},  // O
+                                     {16,  RED},    // P
+                                     {17,  YELLOW}, // Q
+                                     {8,   RED},    // R
+                                     {7,   GREEN},  // S
+                                     {6,   BLUE},   // T
+                                     {5,   YELLOW}, // U
+                                     {4,   RED},    // V
+                                     {3,   GREEN},  // W
+                                     {2,   BLUE},   // X
+                                     {1,   YELLOW}, // Y
+                                     {0,   RED}     // Z
+  };
+
+char* PHRASES[]={"Right Here", "Run", "Happy Halloween", "Cub Scouts", "Mighty Vikings", "Goofy Gators"};
 #define NUM_PHRASES  (sizeof(PHRASES) / sizeof(char*))
+#define SIZE_OF_MAP NUM_PIXEL
 long phraseCounter = 0;
 
 void setup() {
@@ -92,8 +128,9 @@ void setup() {
 }
 
 void loop() {
+
   // Just go in order
-  long phraseOffset = (phraseCounter >= NUM_PHRASES) ? 0 : phraseCounter++;
+  long phraseOffset = phraseCounter++;
   
   // Pick a phrase at random
   //long phraseOffset = random(0, NUM_PHRASES);
@@ -111,7 +148,7 @@ void loop() {
     Serial.println("Light up letter " + String(letter) + " (" + String(letter) + ") at index " + String(letterIndex));
 
     if (letterIndex >= 0) {
-      LIGHT light = PIN_LETTER_MAP[letterIndex];
+      LIGHT light = PIN_LETTER_MAP_BOTTOM_UP[letterIndex];
       //Serial.println("Setting LED " + String(light.addr) + " to R/G/B: " + String(light.color.r & BRIGHTNESS) + "/" + String(light.color.g & BRIGHTNESS) + "/" + String(light.color.b & BRIGHTNESS));
       strip.setPixelColor(light.addr, (light.color.r & BRIGHTNESS), (light.color.g & BRIGHTNESS), (light.color.b & BRIGHTNESS));
       strip.show();
@@ -123,4 +160,60 @@ void loop() {
     delay(BETWEEN_LETTER_PAUSE);
     phrase++;
   }
+
+
+  if (phraseCounter == NUM_PHRASES) {
+    phraseCounter = 0;
+    enablePartyMode();
+  } else {
+    flashAllLights();
+  }
 }
+
+void flashAllLights() {
+  for(int i = 0; i < SIZE_OF_MAP; i++) {
+    LIGHT light = PIN_LETTER_MAP_BOTTOM_UP[i];
+    strip.setPixelColor(light.addr, (light.color.r & BRIGHTNESS), (light.color.g & BRIGHTNESS), (light.color.b & BRIGHTNESS));
+  }
+  
+  strip.show();
+  delay(ON_DURATION);
+
+  for(int i = 0; i < SIZE_OF_MAP; i++) {
+    strip.setPixelColor(i, 0);
+  }
+  strip.show();
+  delay(BETWEEN_LETTER_PAUSE);
+}
+
+void enablePartyMode() {
+  for(int t = 0; t < PARTY_MODE_ITERATIONS; t++) {
+    // How many lights to turn on
+    int numOfLights = (int)random(0, NUM_PIXEL);
+  
+    for(int i = 0; i < numOfLights; i++) {
+      int lightToEnable = (int)random(0, NUM_PIXEL);
+      LIGHT light = PIN_LETTER_MAP_BOTTOM_UP[lightToEnable];
+      strip.setPixelColor(light.addr, (light.color.r & BRIGHTNESS), (light.color.g & BRIGHTNESS), (light.color.b & BRIGHTNESS));
+    }
+    
+    strip.show();
+    delay(ON_DURATION);
+  
+    for(int i = 0; i < SIZE_OF_MAP; i++) {
+      strip.setPixelColor(i, 0);
+    }
+    strip.show();
+    delay(BETWEEN_LETTER_PAUSE);
+  }
+}
+
+void turnOnAllLights() {
+  for(int i = 0; i < SIZE_OF_MAP; i++) {
+    LIGHT light = PIN_LETTER_MAP_BOTTOM_UP[i];
+    strip.setPixelColor(light.addr, (light.color.r & BRIGHTNESS), (light.color.g & BRIGHTNESS), (light.color.b & BRIGHTNESS));
+  }
+  
+  strip.show();
+}
+
